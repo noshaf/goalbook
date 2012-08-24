c9578aa6e220ccbfcbfae6fbc03b10a5ee7af195
Noah Shafi <noshaf@gmail.com>
2012-08-24 13:50:15 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goalbook::Application.config.secret_token = '549e9075444b52a88f0cc6c5a22fa7f08bafdd2f2fb5b1d72eb2c0c66f5ec1eecdd587dc129bf30dd02e0724864ce1b3874b0cf24593d79f4af713ba3249df79'
